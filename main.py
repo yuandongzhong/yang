9dc813d2f207bde675092d2df95e73fcf286ca68
unknown <yuandongzhong>
2022-11-21 17:52:30 +0800

    version 1

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-
-# print(hex(10651698))
-# print(int("A28832", 16))
-
-
 def to_hex(dec_num):
     hex_num = hex(dec_num).replace('0x', '')
     x = 7 - len(hex_num)
@@ -25,7 +20,16 @@ def to_sn(hex_str):
         add = str(add)
     return hex_str + str(add)
 
-for i in range(1, 11):
-    print(i, to_hex(i), to_sn((to_hex(i))))
+# for i in range(1, 11):
+#     print(i, to_hex(i), to_sn((to_hex(i))))
 
+
+if __name__ == "__main__":
+    start = int(input('Start Value: '))
+    end = int(input('End Value: '))
+    
+    with open('sn.txt', 'w') as f:
+        for i in range(start, end):
+            f.write(to_sn((to_hex(i))))
+            f.write('\n')
 
